602215a943b5a5c19260772b009e77d70510c9ea
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-03 19:45:11 -0300

    Update generator.c

--- generator.c
+++ generator.c
@@ -129,7 +129,7 @@ Instruction* generatePowerInstructions(int num1, int num2){
     //loop para fazer a potencia, multiplicação sucessiva
     for(int i = 0; i < num2 ;i++){
     
-        instructions = generateMultiplicationInstructions(int num1, int num2);
+        instructions = generateMultiplicationInstructions(num1, num2);
         //num1 = instructions[].info3;
         
     }
