3d5590b727da1c41b145805d1bac8353bf414f8d
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-03 19:36:45 -0300

    Update generator.h

--- generator.h
+++ generator.h
@@ -7,5 +7,6 @@ Instruction* generateRandomInstructions(int);
 Instruction* generateMultiplicationInstructions(int, int);
 Instruction* readInstructions(char*, int*);
 Instruction* generateDivisionInstructions(int num1, int num2);
+Instruction* generatePowerInstructions(int num1, int num2);
 
 #endif // !GENERATOR_H
