4980f4d2d134fe5e10a99529e47e83062809ff89
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-06-03 22:20:35 -0300

    Update cpu.c

--- cpu.c
+++ cpu.c
@@ -65,10 +65,9 @@ void run(Machine* machine) {
                 break;
               case 3: // sobrescrevendo da numero da memoria ram de um endereço para outro
                 address1 = instruction.info1;
-                address2 = instruction.info2;
-                RAMContent1 = machine->RAM.items[address2];
+                instruction.info2 = machine->RAM.items[address1];
 
-                machine->RAM.items[address1] = RAMContent1;
+                //machine->RAM.items[address1] = RAMContent1;
                 
                 printf("  > Sobrescreve de RAM[%d] (%f) para RAM[%d] .\n", 
                                 address1, RAMContent1, address2);
